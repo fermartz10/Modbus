c2490467bbb208de7fc06ab2859e27683d423e67
Fernando Martinez <35785337+fermartz10@users.noreply.github.com>
2024-02-19 13:14:33 -0600

    Add files via upload

---
+++ guardarDatos.py
@@ -0,0 +1,23 @@
+
+import mysql.connector
+import conexion
+
+conexion.conectar_bd
+
+# Función para guardar los datos en la base de datos
+def guardar_datos_en_bd(data):
+    conn = conexion.conectar_bd()
+    if conn is None:
+        return
+    
+    cursor = conn.cursor()
+    try:
+        for d in data:
+            cursor.execute("INSERT INTO registros (valores) VALUES (%s)", (d,))
+        conn.commit()
+        print("Datos guardados en la base de datos exitosamente")
+    except mysql.connector.Error as err:
+        print("Error al guardar los datos en la base de datos:", err)
+    finally:
+        cursor.close()
+        conn.close()
